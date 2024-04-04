a758fd10d6d50b986dcae4f1f015626dbb7e558b
davidelng <davidelngyt@gmail.com>
2024-04-04 21:49:13 +0200

    add report

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ def main():
     path = 'books/frankenstein.txt'
     text = get_text(path)
     words = count_words(text)
-    letters = count_letters(text)
+    chars = count_chars(text)
+    chars_sorted = sort_chars(chars)
     print(f"--- Begin report of {path}")
     print(f"There are {words} words in this document")
     print()
-    for letter in letters:
-        print(f"The {letter} character was found {letters[letter]} times")
+    for char in chars_sorted:
+        print(f"The \"{char["char"]}\" character was found {char["num"]} times")
     print("--- End report ---")
 
 def get_text(path):
@@ -18,14 +19,25 @@ def count_words(text):
     words = text.split()
     return len(words)
 
-def count_letters(text):
-    letters = {}
+def count_chars(text):
+    chars = {}
     for char in text.lower():
-        if char in letters:
-            letters[char] += 1
+        if char in chars:
+            chars[char] += 1
         else:
-            letters[char] = 1
-    return letters
+            chars[char] = 1
+    return chars
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_chars(chars):
+    chars_list = []
+    for char in chars:
+        if char.isalpha():
+            chars_list.append({"char": char, "num": chars[char]})
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
 
 main()
 
